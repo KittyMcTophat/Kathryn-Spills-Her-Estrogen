e901a43b965f0cfe42cc094879881f47ecaef362
Kitty <kittymctophat@gmail.com>
2023-09-08 12:23:31 -0600

    Fixed Bug With Exported Project
    - Costumes now render properly when the project is exported

--- Characters/Kathryn/CostumeLoader.gd
+++ Characters/Kathryn/CostumeLoader.gd
@@ -3,7 +3,7 @@ extends MeshInstance3D
 
 func _ready():
 	var num_costumes : int = 0;
-	while FileAccess.file_exists("res://Characters/Kathryn/costume" + str(num_costumes) + ".png"):
+	while ResourceLoader.exists("res://Characters/Kathryn/costume" + str(num_costumes) + ".png"):
 		num_costumes += 1;
 	
 	(get_surface_override_material(0) as StandardMaterial3D).uv1_scale.x = (1.0 / float(num_costumes));
