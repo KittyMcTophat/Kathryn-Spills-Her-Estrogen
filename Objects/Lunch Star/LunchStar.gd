bdd8693a2edd6c9bfef2e35e53f405a4542dcfef
Kitty <kittymctophat@gmail.com>
2023-10-05 15:51:33 -0600

    Implement Basic Lunch Star Behavior Still need player interaction and the animation and textures

--- Objects/Lunch Star/LunchStar.gd
+++ Objects/Lunch Star/LunchStar.gd
@@ -2,9 +2,36 @@
 extends Path3D
 class_name LunchStar
 
+@export_enum("Green", "Gold", "Rainbow") var lunch_star_type : String = "Green":
+	set(value):
+		lunch_star_type = value;
+		reload_meshes();
+
+@export var launch_speed : float = 7.0;
+@export var deceleration : float = 1.0;
+@export var end_velocity : float = 3.0;
+
 func _ready():
 	curve_changed.connect(_on_curve_changed);
 	_on_curve_changed();
+	reload_meshes();
+
+func reload_meshes():
+	for child in %"Outer Ring".get_children():
+		child.queue_free();
+	for child in %"Inner Ring".get_children():
+		child.queue_free();
+
+	var new_outer_ring : PackedScene = preload("res://Objects/Lunch Star/green_outer_ring.tscn");
+	var new_inner_ring : PackedScene = preload("res://Objects/Lunch Star/inner_ring.tscn");
+	match (lunch_star_type):
+		"Gold":
+			new_outer_ring = preload("res://Objects/Lunch Star/gold_outer_ring.tscn");
+		"Rainbow":
+			new_outer_ring = preload("res://Objects/Lunch Star/rainbow_outer_ring.tscn");
+	
+	%"Outer Ring".add_child(new_outer_ring.instantiate());
+	%"Inner Ring".add_child(new_inner_ring.instantiate());
 
 func _on_curve_changed():
 	if curve != null && curve.point_count > 0:
@@ -16,14 +43,23 @@ func _on_curve_changed():
 
 func point_mesh_to_curve():
 	if curve != null && curve.get_baked_length() >= curve.bake_interval:
-		$Meshes.basis = curve.sample_baked_with_rotation(curve.bake_interval, true, true).basis;
+		%Meshes.basis = curve.sample_baked_with_rotation(curve.bake_interval, true, true).basis;
 	else:
-		$Meshes.basis = Basis.IDENTITY;
+		%Meshes.basis = Basis.IDENTITY;
 
-func get_path_follow() -> PathFollow3D:
-	var path_follow : PathFollow3D = PathFollow3D.new();
+func get_path_follow() -> LunchStarPathFolow3D:
+	var path_follow : LunchStarPathFolow3D = LunchStarPathFolow3D.new();
 	add_child(path_follow);
 	path_follow.progress = curve.bake_interval;
 	path_follow.loop = false;
 	path_follow.use_model_front = true;
 	return path_follow;
+
+func launch_path_follow(path_follow : LunchStarPathFolow3D):
+	# Play some animation
+	var ls : LunchStarPathFolow3D.LaunchSettings = LunchStarPathFolow3D.LaunchSettings.new();
+	ls.initial_velocity = launch_speed;
+	ls.deceleration = deceleration;
+	ls.end_velocity = end_velocity;
+	ls.total_distance = curve.get_baked_length();
+	path_follow.launch(ls);
---
+++ Objects/Lunch Star/LunchStarPathFollow3D.gd
@@ -0,0 +1,106 @@
+extends PathFollow3D
+class_name LunchStarPathFolow3D
+
+signal launched();
+signal deceleration_started();
+signal launch_finished();
+
+class LaunchSettings:
+	var initial_velocity : float;
+	var deceleration : float;
+	var end_velocity : float;
+	var total_distance : float;
+
+# Passed in by lunch star when launching
+var ls : LaunchSettings = null;
+
+# Calculated when launched
+var deceleration_start_distance : float = 0.0;
+
+var deceleration_time : float = 0.0;
+var total_launch_time : float = 0.0;
+
+var launch_timer : Timer = null;
+var decel_timer : Timer = null;
+
+func launch(_ls : LaunchSettings):
+	self.ls = _ls;
+	calculate_deceleration_start_distance();
+	calculate_time();
+	launched.emit();
+	
+	launch_timer = Timer.new();
+	add_child(launch_timer);
+	launch_timer.process_callback = Timer.TIMER_PROCESS_PHYSICS;
+	launch_timer.wait_time = total_launch_time;
+	launch_timer.one_shot = true;
+	launch_timer.start();
+	
+	decel_timer = Timer.new();
+	add_child(decel_timer);
+	decel_timer.process_callback = Timer.TIMER_PROCESS_PHYSICS;
+	decel_timer.wait_time = total_launch_time - deceleration_time;
+	launch_timer.one_shot = true;
+	decel_timer.start();
+	
+	await decel_timer.timeout;
+	
+	decel_timer.queue_free();
+	decel_timer = null;
+	deceleration_started.emit();
+	
+	await launch_timer.timeout;
+	
+	progress = ls.total_distance;
+	launch_timer.queue_free();
+	launch_timer = null;
+	launch_finished.emit();
+
+func calculate_deceleration_start_distance():
+	var v_f_squared : float = ls.end_velocity * ls.end_velocity;
+	var v_i_squared : float = ls.initial_velocity * ls.initial_velocity;
+	
+	var numerator : float = v_f_squared - v_i_squared;
+	var denominator : float = 2 * (-ls.deceleration);
+	
+	var deceleration_distance : float;
+	if denominator != 0:
+		deceleration_distance = numerator / denominator;
+	else:
+		deceleration_distance = 0.0;
+	
+	deceleration_start_distance = ls.total_distance - deceleration_distance;
+
+func calculate_time():
+	if ls.deceleration == 0:
+		deceleration_time = 0;
+	else:
+		deceleration_time = (ls.end_velocity - ls.initial_velocity) / (-ls.deceleration);
+	
+	total_launch_time = deceleration_start_distance / ls.initial_velocity;
+	total_launch_time += deceleration_time;
+
+func _physics_process(delta):
+	if launch_timer == null:
+		return;
+	
+	progress = calculate_progress();
+
+func calculate_progress() -> float:
+	if elapsed_time() < (total_launch_time - deceleration_time):
+		return ls.initial_velocity * elapsed_time();
+	elif deceleration_time != 0:
+		var prog : float = ls.initial_velocity * (total_launch_time - deceleration_time)
+
+		var decelerate_time_passed : float = (elapsed_time() - (total_launch_time - deceleration_time));
+		var cur_velocity : float = (ls.initial_velocity - (ls.deceleration * decelerate_time_passed));
+		var numerator : float = ((cur_velocity * cur_velocity) - (ls.initial_velocity * ls.initial_velocity));
+		var denominator : float = (2 * -ls.deceleration);
+		prog += (numerator / denominator);
+		
+		return prog;
+	else:
+		return ls.total_distance;
+
+func elapsed_time():
+	return total_launch_time - launch_timer.time_left;
